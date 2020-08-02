0b8bd2250e3eced3b71fb08d653c840767dd29a2
andreasowens <69100282+andreasowens@users.noreply.github.com>
2020-08-01 22:21:57 -0700

    Add files via upload

---
+++ build/precache-manifest.fb0d97a502345c4637e538632dd4b4c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffb02c0d19ff8da17ba0755a286d519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f577d0922ddb6dfcf919",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "a60307c7efe9ababc67e",
+    "url": "/static/js/2.7c24ba84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7c24ba84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f577d0922ddb6dfcf919",
+    "url": "/static/js/main.1386a350.chunk.js"
+  },
+  {
+    "revision": "f8ce95e23ae4eb434ece",
+    "url": "/static/js/runtime-main.ef835f46.js"
+  },
+  {
+    "revision": "3b50b55c0e9e69e3568fd1a068dcbc58",
+    "url": "/static/media/layloRed.3b50b55c.png"
+  }
+]);
